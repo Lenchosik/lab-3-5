213097e68ffc5d29a747131735848e68d4a4ef27
Klishnikova <134382996+Lenchosik@users.noreply.github.com>
2024-10-28 22:17:52 +0300

    Add files via upload

---
+++ blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from blog.models import Article, Comment
+
+# Register your models here.
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, PostAdmin)
---
+++ main/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from main.forms import LoginForm, RegisterForm
+
+# страница пользователя
+def me(request):
+    # если не авторизован, то редирект на страницу входа
+    if not request.user.is_authenticated:
+        return redirect('login')
+    # рендерим шаблон и передаем туда объект пользователя
+    return render(request, 'main/me.html', {'user': request.user})
+
+# выход
+def doLogout(request):
+    # вызываем функцию django.contrib.auth.logout и делаем редирект на страницу входа
+    logout(request)
+    return redirect('login')
+
+# страница входа
+def loginPage(request):
+
+    # инициализируем объект класса формы
+    form = LoginForm()
+
+    # обрабатываем случай отправки формы на этот адрес
+    if request.method == 'POST':
+
+        # заполянем объект данными, полученными из запроса
+        form = LoginForm(request.POST)
+
+        # проверяем валидность формы
+        if form.is_valid():
+            # пытаемся авторизовать пользователя
+            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                # если существует пользователь с таким именем и паролем,
+                # то сохраняем авторизацию и делаем редирект
+                login(request, user)
+                return redirect('me')
+            else:
+                # иначе возвращаем ошибку
+                form.add_error(None, 'Неверные данные!')
+
+    # рендерим шаблон и передаем туда объект формы
+    return render(request, 'main/login.html', {'form': form})
+
+# регистрация
+def registerPage(request):
+
+    # инициализируем объект формы
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        # заполняем объект данными формы, если она была отправлена
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            # если форма валидна - создаем нового пользователя
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+
+            return redirect('login')
+    # ренедерим шаблон и передаем объект формы
+    return render(request, 'main/registration.html', {'form': form})
+
